7321e3d2341525f2205c2fa0bce9f249899ea57a
Raja Kumar <rajakumar@Rajas-MacBook-Air.local>
2024-10-15 21:25:53 +0530

    Added toLower functionality

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -6,26 +6,42 @@ export default function TextForm(props) {
     let newText = text.toUpperCase();
     setText(newText);
   }
+  const handleLoClick = ()=>{
+    // console.log("UpperCase was clicked: " + text);
+    let newText = text.toLowerCase();
+    setText(newText);
+  }
   const handleOnChange = (event)=>{
     // console.log("On change ");
     setText(event.target.value);
   }
-  const [text, setText] = useState('Enter text here');
+
+  const [text, setText] = useState('');
   // text = "new text" // wrong way to change the state
   // setText("new Text"); // correct way to change the state
   return (
-    <div>
-      <h1>{props.heading}</h1>
-      <div className="mb-3">
-        <textarea
-          className="form-control"
-          value={text}
-          onChange={handleOnChange}
-          id="myBox"
-          rows="8"
-        ></textarea>
+    <>
+      <div className="container">
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            value={text}
+            onChange={handleOnChange}
+            id="myBox"
+            rows="8"
+          ></textarea>
+        </div>
+        <button className="btn btn-primary mx-1" onClick={handleUpClick}>Covert to UpperCase</button>
+        <button className="btn btn-primary mx-1" onClick={handleLoClick}>Covert to LowerCase</button>
       </div>
-      <button className="btn btn-primary" onClick={handleUpClick}>Covert to UpperCase</button>
-    </div>
+      <div className="container">
+        <h2>Your text summary</h2>
+        <p>{text.split(" ").length} words and {text.length} characters</p>
+        <p>{0.008 * text.split(" ").length } Minutes read</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+      </div>
+    </>
   );
 }
